5919884246fdd7ebf5914750c7e8eb85c9459f0f
Libertybanshee <Libertybanshee1@gmail.com>
2020-09-14 20:04:01 +0200

    ajout exercice

---
+++ escalier2.js
@@ -0,0 +1,14 @@
+const [nd,file , nb] = process.argv;
+
+function scale(x) {
+    let i = 0; 
+        while (i < x) {
+        space = x - i;
+        diese = i + 1;
+        scale = " ".repeat(space) + "#".repeat(diese);
+        console.log(scale);
+        i++;
+    }
+}
+
+scale(nb);
