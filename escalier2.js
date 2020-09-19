6e146166f45530574abf9cc3443015c91712e366
Libertybanshee <Libertybanshee1@gmail.com>
2020-09-19 12:10:26 +0200

    exercice repetition

--- escalier2.js
+++ escalier2.js
@@ -1,14 +1,14 @@
-const arg = process.argv[2];
+const nb = process.argv[2];
 
-function scale(x) {
-    let i = 0;
-        while (i < x) {
-            space = x - i;
-            diese = i + 1;
-            draw = " ".repeat(space) + "#".repeat(diese);
-            console.log(draw);
-            i++;  
-        }
-}
+function scale(x) { 
+    i = 0
+    while (i < x) {
+        space = x - i;
+        diese = i + 1;
+        scale = " ".repeat(space) + "#".repeat(diese);
+        console.log(scale);
+        i++; 
+    }
+};
 
-scale(arg);
+scale(nb);
