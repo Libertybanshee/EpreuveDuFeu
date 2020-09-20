1638b7ab844370d24dde7dc78a5732be9d154231
Libertybanshee <Libertybanshee1@gmail.com>
2020-09-20 08:55:27 +0200

    exercice try1 du 20_09_20

--- escalier2.js
+++ escalier2.js
@@ -1,14 +1,14 @@
 const nb = process.argv[2];
 
-function scale(x) { 
+function scale(x) {
     i = 0
-    while (i < x) {
+    while ( i < x) {
         space = x - i;
         diese = i + 1;
         scale = " ".repeat(space) + "#".repeat(diese);
         console.log(scale);
-        i++; 
+        i++;
     }
-};
+}
 
 scale(nb);
