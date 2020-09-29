7f8e9891b4a077aff8a55dde8dbfbd469a46d784
Libertybanshee <Libertybanshee1@gmail.com>
2020-09-29 11:09:18 +0200

    repetition 28_09

--- escalier2.js
+++ escalier2.js
@@ -1,14 +1,14 @@
 const nMarche = process.argv[2];
 
-function Escalier(x) {
+function MakerMarche (x) {
     i = 0;
     while (i < x) {
         space = x - i;
         diese = i + 1;
-        construction = " ".repeat(space) + "#".repeat(diese);
-        console.log(construction);
+        Calcul = " ".repeat(space) + "#".repeat(diese);
+        console.log(Calcul);
         i++;
     }
 }
 
-Escalier(nMarche);
+MakerMarche (nMarche);
