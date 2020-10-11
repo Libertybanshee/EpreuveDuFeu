e0952ad5a4ae55c459f4898cdf84cd1e0ee57adf
Libertybanshee <Libertybanshee1@gmail.com>
2020-10-11 13:04:41 +0200

    exercide du 11_10_19

--- escalier3.js
+++ escalier3.js
@@ -1,8 +1,8 @@
 const Arg = process.argv[2];
 
-for (i = 0; i < Arg; i++) {
+for (i=0; i < Arg ;i++) {
     space = Arg - i;
     diese = i + 1;
-    calcule = " ".repeat(space) + "#".repeat(diese);
-    console.log(calcule);
+    scaleMaker = " ".repeat(space) + "#".repeat(diese);
+    console.log(scaleMaker);
 }
--- majuscule2.js
+++ majuscule2.js
@@ -5,12 +5,12 @@ let j = 0;
 
 for (i=0; i < Arg.length; i++) {
     j++;
-    if (Lettre[i] != " ") {
+    if ( Lettre[i] != " ") {
         if (j % 2 == 0) {
-            Print += Lettre[i].toLowerCase();
+            Print += Lettre[i].toUpperCase();
         }
         else {
-            Print += Lettre[i].toUpperCase();
+            Print += Lettre[i].toLowerCase();
         }
     }
     else {
