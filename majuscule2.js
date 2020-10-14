8560d5df17ad945d5c8f5968d9fc4203d6d80298
Libertybanshee <Libertybanshee1@gmail.com>
2020-10-14 23:11:57 +0200

    exercice2 du 13 - 10 - 20

--- majuscule2.js
+++ majuscule2.js
@@ -1,18 +1,18 @@
 const Arg = process.argv[2];
 var Lettre = Arg.split('');
 let Print = "";
-j = 0;
+let j = 0;
 
-for (i=0; i < Arg.length ;i++) {
+for (i = 0; i < Arg.length; i++) {
     j++;
     if (Lettre[i] != " ") {
-        if(j % 2 == 0) {
+        if (j % 2 == 0) {
             Print += Lettre[i].toLowerCase();
         }
         else {
             Print += Lettre[i].toUpperCase();
         }
-    } 
+    }
     else {
         Print += " ";
     }
