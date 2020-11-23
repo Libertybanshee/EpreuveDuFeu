4fdb45d61097ff625121299105433f7707343987
Libertybanshee <Libertybanshee1@gmail.com>
2020-11-23 23:20:21 +0100

    repetition edf 23_11_20

--- escalier5.js
+++ escalier5.js
@@ -1,8 +1,8 @@
 const Arg = process.argv[2];
 
-for (i=0; i < Arg; i++){
-    Space = Arg - i;
+for(i=0; i < Arg; i++){
     Diese = i + 1;
+    Space = Arg - i;
     MarcheMaker = " ".repeat(Space) + "#".repeat(Diese);
     console.log(MarcheMaker);
-}
+};
