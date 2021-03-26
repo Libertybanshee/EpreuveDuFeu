08a0810f6967c6b743c164a5ae9e65ec540190b7
Libertybanshee <65285497+Libertybanshee@users.noreply.github.com>
2021-03-26 11:48:14 +0100

    maj du 26_03_21

--- escalier5.js
+++
@@ -1,8 +0,0 @@
-const Arg = process.argv[2];
-
-for(i=0; i < Arg; i++){
-    diese = i + 1;
-    space = Arg - i;
-    MarcheMaker = " ".repeat(space) + "#".repeat(diese);
-    console.log(MarcheMaker);
-}
